01212cef547d061abc596bc09555c5945ee76ad8
jczamora-git <johnchristopherkingzamora@gmail.com>
2026-09-14 22:46:02 +0800

    Update application identity to JC Photo Gallery and package ID to com.jc.photogallery

--- android/app/src/main/java/com/jc/photogallery/MainActivity.java
+++ android/app/src/main/java/com/jc/photogallery/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.jc.photogallery;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'photo-gallery',
+  appId: 'com.jc.photogallery',
+  appName: 'JC Photo Gallery',
   webDir: 'dist',
 };
 
